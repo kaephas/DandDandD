4f38861fca7607e6e23e98040bfb8f33833f700d
Kaephas <kkain@mail.greenriver.edu>
2019-06-01 02:03:45 -0400

    fix table resizing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,41 @@
-// $(document).ready( function () {
-//     $('#drinkTable').DataTable();
-// } );
+$(document).ready( function () {
+    $('#drinkTable').DataTable( {
+        "oLanguage": {
+            "oPaginate": {
+                "sPrevious": "<",
+                "sNext": ">"
+            }
+        }
+    });
 
-$(".clickableRow").click(function() {
+    $("#drinkTable tr").css('cursor', 'pointer');
+    $("#headerRow").css('cursor', 'n-resize');
+
+} );
+
+let row = $(".clickableRow");
+
+// $("#drinkTable tr").hover(function() {
+// });
+
+row.click(function() {
     window.location = $(this).attr('data-href');
 });
-
-$(document).ready(function() {
-   let table = $('#drinkTable').DataTable( {
-       rowReorder: {
-           selector: 'td:nth-child(2)'
-       },
-       responsive: true,
-       "oLanguage": {
-           "oPaginate": {
-               "sPrevious": "<",
-               "sNext": ">"
-           }
-       },
-
-   });
-    let firstChild = $('#drinkTable_previous:first-child');
-       // firstChild.children('a:first').html('Prev');
-});
+//
+// $(document).ready(function() {
+//    let table = $('#drinkTable').DataTable( {
+//        rowReorder: {
+//            selector: 'td:nth-child(2)'
+//        },
+//        responsive: true,
+//        "oLanguage": {
+//            "oPaginate": {
+//                "sPrevious": "<",
+//                "sNext": ">"
+//            }
+//        },
+//
+//    });
+//     let firstChild = $('#drinkTable_previous:first-child');
+//        // firstChild.children('a:first').html('Prev');
+// });
